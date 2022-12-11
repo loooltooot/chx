7e0aa9a2867c5e092b0c61179ca088ac060e08c8
Emir Zakrewski <frenkchambers@gmail.com>
2022-12-11 15:29:05 +0300

    0x111222[fonts optimization and db created]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
 import type { AppProps } from 'next/app'
 import '../styles/normalize.css'
 import '../styles/global.scss'
+import { Crimson_Text, UnifrakturCook } from '@next/font/google'
+
+const crimsonText = Crimson_Text({
+    weight: ['400', '700'],
+    style: ['normal']
+})
+
+const unifrakturCook = UnifrakturCook({
+    weight: ['700'],
+    style: ['normal']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Component {...pageProps} />
+        <>
+            <style jsx global>{`
+                :root {
+                    --font-crimson-text: ${crimsonText.style.fontFamily};
+                    --font-unifraktur-cook: ${unifrakturCook.style.fontFamily};
+                }
+            `}</style>
+            <Component {...pageProps} />
+        </>
     )
 }
